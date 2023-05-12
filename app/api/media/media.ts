9674f5fcc724483136e99753e9b8cc892bcfebea
BoreBo <Patharadach.ma@kkumail.com>
2023-05-12 19:28:18 +0700

    try upload image

--- app/api/media/media.ts
+++ app/api/media/media.ts
@@ -4,16 +4,46 @@ import jsonRead from "../json-read-format";
 import path from 'path';
 var express = require("express");
 var router = express.Router();
-var cors = require("cors");
+// var cors = require("cors");
 var app = express();
-app.use(cors());
+// app.use(cors());
 const prisma = new PrismaClient();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+// import multer from 'multer';
 
-const mediaPath = path.join(__dirname, 'media');
+const upload = multer({
+    dest: 'uploads/',
+    fileFilter: (req:Request, file:File, cb:Function) => {
+      // File filter logic (optional)
+      // For example, you can restrict the file types or file size
+      cb(null, true);
+    }
+  });
+  
+  // File
 
-router.use('/media', express.static(mediaPath));
-
-// Your other routes and configurations...
-
+// File upload endpoint
+app.post('/upload', upload.single('image'), async (req: Request, res: Response) => {
+    // Access the uploaded file details from `req.file`
+    if (!req.file) {
+      res.status(400).send('No file uploaded');
+      return;
+    }
+  
+    console.log(req.file);
+  
+    // Process and save the file using Prisma
+    // const { filename, path } = req.file;
+    // const uploadedFile = await prisma.file.create({
+    //   data: {
+    //     filename,
+    //     path
+    //   }
+    // });
+  
+    res.send('File uploaded successfully');
+  });
+  
 
 export default router;
