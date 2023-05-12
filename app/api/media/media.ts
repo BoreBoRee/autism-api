7957b43aa2ca24894e216616cc6bd95c6a72ec6f
BoreBo <Patharadach.ma@kkumail.com>
2023-05-12 20:08:34 +0700

    try upload

--- app/api/media/media.ts
+++ app/api/media/media.ts
@@ -1,26 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
 import jsonRead from "../json-read-format";
-import path from 'path';
+
 var express = require("express");
 var router = express.Router();
 // var cors = require("cors");
 var app = express();
 // app.use(cors());
+import path, { PlatformPath } from 'path';
+import { promises as fs } from 'fs';
 const prisma = new PrismaClient();
 const bodyParser = require('body-parser');
 const multer = require('multer');
 // import multer from 'multer';
 
-const upload = multer.diskStorage({
-    dest: 'uploads/',
-    fileFilter: (req:Request, file:File, cb:Function) => {
-      // File filter logic (optional)
-      // For example, you can restrict the file types or file size
-      cb(null, true);
-    }
+const storage = multer.diskStorage({
+    destination: 'uploads/', // Specify the destination directory
+    filename: (req:Request, file:File, cb:Function) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      const extension = path.extname(file.name);
+      cb(null, uniqueSuffix + extension);
+    },
   });
-  
+  const upload = multer({ storage });
   // File
 
 // File upload endpoint
@@ -32,7 +34,9 @@ router.post('/upload', upload.single('image'), async (req: Request, res: Respons
     }
   
     console.log(req.file);
-  
+    const { filename, path: filePath, originalname } = req.file;
+    // const newPath = path.replace('uploads/', 'new-directory/');
+    // fs.renameSync(path, newPath);
     // Process and save the file using Prisma
     // const { filename, path } = req.file;
     // const uploadedFile = await prisma.file.create({
