249a92492eaa2073b74fdf3a2a883deee0d2bf53
BoreBo <Patharadach.ma@kkumail.com>
2023-05-12 21:17:39 +0700

    s2

--- app/api/media/media.ts
+++ app/api/media/media.ts
@@ -1,55 +1,91 @@
-import { Request, Response, NextFunction } from "express";
-import { PrismaClient } from "@prisma/client";
-import jsonRead from "../json-read-format";
 
-var express = require("express");
-var router = express.Router();
-// var cors = require("cors");
-var app = express();
-// app.use(cors());
+import express, { Request, Response } from 'express';
+import multer, { Multer } from 'multer';
 import path, { PlatformPath } from 'path';
 import { promises as fs } from 'fs';
-const prisma = new PrismaClient();
-const bodyParser = require('body-parser');
-const multer = require('multer');
+// import prisma from './prisma';
+
+
+var router = express.Router();
+// var cors = require("cors");
+
+// app.use(cors());
+
 // import multer from 'multer';
 
+const app = express();
+
+// Set up Multer middleware for handling file uploads
 const storage = multer.diskStorage({
-    destination: 'uploads/', // Specify the destination directory
-    fileFilter: (req:Request, file:File, cb:Function) => {
-    // filename: (req:Request, file:File, cb:Function) => {
-    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    //   const extension = path.extname(file.name);
-    //   cb(null, uniqueSuffix + extension);
-      cb(null, true);
-    },
-  });
-  const upload = multer({ storage });
-  // File
+  destination: 'uploads/', // Specify the destination directory
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const extension = path.extname(file.originalname);
+    cb(null, uniqueSuffix + extension);
+  },
+});
+const upload = multer({ storage });
 
 // File upload endpoint
-router.post('/upload', upload.single('image'), async (req: Request, res: Response) => {
-    // Access the uploaded file details from `req.file`
-    if (!req.file) {
-      res.status(400).send('No file uploaded');
-      return;
-    }
+app.post('/upload', upload.single('image'), async (req: Request, res: Response) => {
+  if (!req.file) {
+    res.status(400).send('No file uploaded');
+    return;
+  }
+
+  // Process and save the file using Prisma
+  const { filename, path: filePath, originalname } = req.file;
+//   const uploadedFile = await prisma.file.create({
+//     data: {
+//       filename,
+//       path: filePath,
+//       originalname,
+//     },
+//   });
+
+  // Move the uploaded file to a different directory
+  const newPath = filePath.replace('uploads/', 'new-directory/');
+  await fs.rename(filePath, newPath);
+
+  res.send('File uploaded successfully');
+});
+
+// const storage = multer.diskStorage({
+//     destination: 'uploads/', // Specify the destination directory
+//     fileFilter: (req:Request, file:File, cb:Function) => {
+//     // filename: (req:Request, file:File, cb:Function) => {
+//     //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     //   const extension = path.extname(file.name);
+//     //   cb(null, uniqueSuffix + extension);
+//       cb(null, true);
+//     },
+//   });
+//   const upload = multer({ storage });
+//   // File
+
+// // File upload endpoint
+// router.post('/upload', upload.single('image'), async (req: Request, res: Response) => {
+//     // Access the uploaded file details from `req.file`
+//     if (!req.file) {
+//       res.status(400).send('No file uploaded');
+//       return;
+//     }
   
-    console.log(req.file);
-    const { filename, path: filePath, originalname } = req.file;
-    // const newPath = path.replace('uploads/', 'new-directory/');
-    // fs.renameSync(path, newPath);
-    // Process and save the file using Prisma
-    // const { filename, path } = req.file;
-    // const uploadedFile = await prisma.file.create({
-    //   data: {
-    //     filename,
-    //     path
-    //   }
-    // });
+//     console.log(req.file);
+//     const { filename, path: filePath, originalname } = req.file;
+//     // const newPath = path.replace('uploads/', 'new-directory/');
+//     // fs.renameSync(path, newPath);
+//     // Process and save the file using Prisma
+//     // const { filename, path } = req.file;
+//     // const uploadedFile = await prisma.file.create({
+//     //   data: {
+//     //     filename,
+//     //     path
+//     //   }
+//     // });
   
-    res.send('File uploaded successfully');
-  });
+//     res.send('File uploaded successfully');
+//   });
   
 
 export default router;
