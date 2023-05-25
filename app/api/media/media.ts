a79d875538f24a8c9b0f57b7f0c5b3a289ea0029
BoreBo <Patharadach.ma@kkumail.com>
2023-05-25 19:20:30 +0700

    graph data

--- app/api/media/media.ts
+++ app/api/media/media.ts
@@ -5,37 +5,11 @@ import path, { PlatformPath } from 'path';
 import { promises as fs } from 'fs';
 import { createReadStream } from 'fs';
 import stream from 'stream';
-// import prisma from './prisma';
-
-
+import { PrismaClient } from "@prisma/client";
+import jsonRead from "../json-read-format";
+const prisma = new PrismaClient();
 var router = express.Router();
-// var cors = require("cors");
-
-// app.use(cors());
-
-// import multer from 'multer';
-
 const app = express();
-// const DIR = `.${process.env.UPLOAD_PATH}`;
-// Set up Multer middleware for handling file uploads
-// let storage = multer.diskStorage({
-//     destination: async (req, file, cb) => {
-//         const uploadPath = path.join(__dirname, 'uploads');
-//         try {
-//           await fs.access(uploadPath);
-//           console.log("file are exist");
-//         } catch (error) {
-//           await fs.mkdir(uploadPath);
-//           console.log("file are NOT exist");
-//         }
-//         cb(null, uploadPath);
-//       },
-//     filename: (req, file, cb) => {
-//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//       const extension = path.extname(file.originalname);
-//       cb(null, uniqueSuffix + extension);
-//     },
-//   });
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       const uploadPath = path.join(__dirname, '/images');
@@ -48,31 +22,25 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // File upload endpoint
-router.post('/upload', upload.single('image'), async (req: Request, res: Response) => {
+router.post('/upload/:screening_id', upload.single('image'), async (req: Request, res: Response) => {
+  const screening_id = req.params.screening_id;
   if (!req.file) {
     res.status(400).send('No file uploaded');
     return res.send({ success: false });
   }
   else {
     console.log("file received",req.file);
+    const uploadedFile = await prisma.screening_comments.updateMany({
+      where: {
+        screening_id: Number(screening_id),
+      },
+      data: {
+        image_file_name: req.file.filename,
+      },
+    });
     return res.send({ success: true, filename: req.file.filename });}
-  // Process and save the file using Prisma
-  // const { filename, path: filePath, originalname } = req.file;
-//   const uploadedFile = await prisma.file.create({
-//     data: {
-//       filename,
-//       path: filePath,
-//       originalname,
-//     },
-//   });
-
-  // Move the uploaded file to a different directory
-//   const newPath = filePath.replace('uploads/', 'new-directory/');
-//   await fs.rename(filePath, newPath);
-  // console.log('File uploaded:', filePath);
-
-  // res.send('File uploaded successfully');
 });
+
 router.get('/uploads/:fileimage', async (req: Request, res: Response) => {
   var filepath = path.resolve('./././images', req.params.fileimage);
   filepath = filepath.replace(/\.[%/.]+$/, ".webp");
